a3d542de2d1b21898b16a90da9c4c7d871b80fd2
Alessandro Barbagallo <alessbarb@gmail.com>
2023-08-20 12:58:10 +0200

    Create project_init.sh

---
+++ scripts/project_init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Script to initialize a new Node.js project and establish directory structure
+
+# Run npm init to initialize a new project
+npm init -y
+
+# Create directories
+mkdir src tests config public utils
+
+echo "Project initialized and directory structure established."
