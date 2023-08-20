ccc3dad3511105e5861c55c9dbdf0a9c0d4ad8b1
Alessandro Barbagallo <alessbarb@gmail.com>
2023-08-20 13:19:46 +0200

    Create 4_env_vars.sh

---
+++ scripts/4_env_vars.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Install dotenv
+npm install dotenv
+
+# Create a .env file for local environment variables
+touch .env
+
+# Create a .env.example file as a template
+touch .env.example
+
+echo "dotenv installed"
